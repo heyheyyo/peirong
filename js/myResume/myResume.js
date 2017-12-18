3c8084a3ffe8e6cf87c6af322e6135766c5ba12b
pei-rong chen <xiaorongyo@gmail.com>
2017-12-19 04:01:13 +0800

    all over

--- js/myResume/myResume.js
+++ js/myResume/myResume.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-	
+	$('.skill-detail, .education-detail, .expect-job-detail, .language-detail, .project-detail').hide();
+
+
+	$('.skill-top').click(function(){		
+		$(this).parent().find('.skill-detail').slideToggle();
+
+	});
+
+	$('.education-top').click(function(){
+		$(this).parent().find('.education-detail').slideToggle();
+	});
+
+	$('.expect-job-top').click(function(){
+		$(this).parent().find('.expect-job-detail').slideToggle();
+	});
+
+	$('.language-top').click(function(){
+		$(this).parent().find('.language-detail').slideToggle();
+	});
+
+	$('.project-top').click(function(){
+		$(this).parent().find('.project-detail').slideToggle();
+	});
+
 });
